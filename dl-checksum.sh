89b2adec124fd0bc4620a86278155bf68a09a97d
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-21 15:23:14 -0500

    cleanup dl-checksums.sh

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,48 +1,48 @@
 #!/usr/bin/env sh
 APP=packer
-VER=${1:-1.4.5}
 DIR=~/Downloads
-MIRROR=https://releases.hashicorp.com/${APP}/${VER}
-
-CHECKSUMS=${APP}_${VER}_SHA256SUMS
-LCHECKSUMS=$DIR/$CHECKSUMS
-RCHECKSUMS=$MIRROR/$CHECKSUMS
-
-if [ ! -e $DIR/$CHECKSUMS ];
-then
-    wget -q -O $LCHECKSUMS $RCHECKSUMS
-fi
-
+MIRROR=https://releases.hashicorp.com/${APP}
 
 dl()
 {
-    local os=$1
-    local arch=$2
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
     local platform=${os}_${arch}
-    local file=${APP}_${VER}_${platform}.zip
-    local url=$MIRROR/$file
+    local file=${APP}_${ver}_${platform}.zip
+    local url=$MIRROR/$ver/$file
 
-    printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform `grep $file $LCHECKSUMS | awk '{print $1}'`
+    printf "        # %s\n" $url
+    printf "        %s: sha256:%s\n" $platform `fgrep $file $lchecksums | awk '{print $1}'`
 }
 
-printf "  # %s\n" $RCHECKSUMS
-printf "  '%s':\n" $VER
-dl darwin 386
-dl darwin amd64
-dl freebsd 386
-dl freebsd amd64
-dl linux 386
-dl linux amd64
-dl linux arm
-dl linux arm64
-dl linux mips
-dl linux mips64
-dl linux mipsle
-dl linux ppc64le
-dl linux s390x
-dl openbsd 386
-dl openbsd amd64
-dl solaris amd64
-dl windows 386
-dl windows amd64
+dl_all () {
+    local ver=$1
+    local checksums=${APP}_${ver}_SHA256SUMS
+    local lchecksums=$DIR/$checksums
+    local rchecksums=$MIRROR/$ver/$checksums
+
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $rchecksums
+    fi
+
+    printf "      # %s\n" $rchecksums
+    printf "      '%s':\n" $ver
+    dl $ver $lchecksums darwin 386
+    dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums freebsd 386
+    dl $ver $lchecksums freebsd amd64
+    dl $ver $lchecksums linux 386
+    dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums linux arm
+    dl $ver $lchecksums linux arm64
+    dl $ver $lchecksums openbsd 386
+    dl $ver $lchecksums openbsd amd64
+    dl $ver $lchecksums solaris amd64
+    dl $ver $lchecksums windows 386
+    dl $ver $lchecksums windows amd64
+}
+
+dl_all 1.4.5
